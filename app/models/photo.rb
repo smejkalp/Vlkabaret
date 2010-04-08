18aeb6c1a56298f39d1ba7dfd8b408dd2704db8a
Petr Smejkal <xmejkal@gmail.com>
2010-04-08 19:42:03 +0200

    deleted imgae files

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,5 +1,5 @@
 class Photo < ActiveRecord::Base      
   belongs_to :concert
   
-  has_attached_file :image, :styles => { :thumb => "150x150>", :normal => "400x400>"}
+  has_attached_file :image, :styles => { :thumb => "150x150>", :normal => "600x600>"}
 end
