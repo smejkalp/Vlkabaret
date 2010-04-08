856855c4e5ad0070d6af966fc6d925cbf2d1d3e5
Petr Smejkal <xmejkal@gmail.com>
2010-04-08 18:25:10 +0200

    simple gallery

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,6 +1,5 @@
 class Photo < ActiveRecord::Base      
   belongs_to :concert
   
-  has_attached_file :image
-  
+  has_attached_file :image, :styles => { :thumb => "150x150>", :normal => "400x400>"}
 end
--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20100331101022) do
+ActiveRecord::Schema.define(:version => 20100408160736) do
 
   create_table "clubs", :force => true do |t|
     t.string   "name"
@@ -50,6 +50,10 @@ ActiveRecord::Schema.define(:version => 20100331101022) do
     t.integer  "concert_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
   end
 
   create_table "roles", :force => true do |t|
